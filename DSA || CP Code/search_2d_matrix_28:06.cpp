89143591accf04fe2500f3fbdbb663bea22e0f65
Vedic <65288795+vedic529@users.noreply.github.com>
2021-06-28 20:06:45 +0530

    Add files via upload

---
+++ DSA || CP Code/search_2d_matrix_28:06.cpp
@@ -0,0 +1,26 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 class Solution \{\
+public:\
+    bool searchMatrix(vector<vector<int>>& matrix, int target) \{\
+        int n=matrix.size();\
+        int m=matrix[0].size();\
+        int i=0;int j=m-1;\
+        while(i<n && j>=0)\{\
+            if(matrix[i][j]==target)\{\
+                return true;\
+            \}else if(matrix[i][j]< target)\{\
+                i++;\
+            \}else\{\
+                j--;\
+            \}\
+\}\
+        return false;\
+        \
+    \}\
+\};}
